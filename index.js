f86ee42af31b59b371da44b47d7748dfa0af0ea9
Fernando Lopez <fer.ragnar19@gmail.com>
2018-02-19 14:50:44 -0300

    fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const
 	express = require('express'),
 	bodyParser = require('body-parser'),
+	request = require('request'),
 	app = express().use(bodyParser.json()); // creamos express server
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook funciona!'));
@@ -59,6 +60,16 @@ app.get('/webhook', (req, res) => {
 });
 
 function handleMessage(sender_psid, received_message) {
+	let response;
+
+	if (received_message.text){
+		response = {
+			"text": 'You sent the message: "${received_message.text}". Now send me an image!'
+		}
+
+	}
+
+	callSendAPI(sender_psid, response);
 
 }
 
@@ -67,7 +78,26 @@ function handlePostback(sender_psid, received_postback) {
 
 }
 
-// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }

3aafcbd3f59764aab30756cdf2a22ede98d89d7e
Fernando Lopez <fer.ragnar19@gmail.com>
2018-02-19 13:31:35 -0300

    fix

--- index.js
+++ index.js
@@ -24,22 +24,24 @@ app.post('/webhook', (req, res) => {
 	}
 });
 
+//Verificamos token
 app.get('/webhook', (req, res) => {
-	
-	//agregamos token para verificar
-	let VERIFY_TOKEN = "<EAAEZBSgLMwiUBAKOCDL4o3HkDJy7jCz9GHZCvMEwrEOviKoHD3fBinTG0hFeZAtYeZA5lRZCZAOZC7NKh8jnaj6w4Qa9Ecui3K6MoDdZCMeoIzHvYidz9gJuGYTgZCLOkQIoFfOADMf1WKoQI8ZB25eiKDjTNXYGc3AnrkowLuAIKZBogZDZD>"
 
-	let mode = req.query['hub.mode'];
-	let token = req.query['hub.verify_token'];
-	let challenge = req.query['hub.challenge'];
-
-
-	if (mode && token){
-		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-			console.log('WEBHOOK_VERIFIED');
-			res.status(200).send(challenge);
-		} else {
-			res.sendStatus(403);
-		}
-	}
+  let VERIFY_TOKEN = "<EAAEZBSgLMwiUBAGUFfHzkUgcUV56ENElxkm3PaflY3dVjonFe3mu1e0K1LHCZACpZAjeyvoFHZCB5sQZAlnZAFBCqJjtZAKQZCHGeaZBy6ZChvaknKxv8FJzXt4ZAB4g9W3zCZAOveuR2RKb2RfNVknTEZCijmOA91XhGbKZAA4mL4q65F0AZDZD>"
+    
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  if (mode && token) {
+  
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      res.sendStatus(403);      
+    }
+  }
 });

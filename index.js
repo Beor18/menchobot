54b8a9ef4c3cdd01402c97fd26ea053a40611a89
Fernando Lopez <fer.ragnar19@gmail.com>
2018-02-19 13:21:02 -0300

    cambios

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-'use strict'
+// Creamos servidor
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+'use strict';
 
-app.set('port', (process.env.PORT || 5000))
+const
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	app = express().use(bodyParser.json()); // creamos express server
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.listen(process.env.PORT || 1337, () => console.log('webhook funciona!'));
 
-// Process application/json
-app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+	let body = req.body;
 
-// Index route
-app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
-})
+	if (body.object === 'page') {
+		body.entry.forEach(function(entry){
+			let webhook_event = entry.messaging[0];
+			console.log(webhook_event);
+		});
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'EAAEZBSgLMwiUBAKOCDL4o3HkDJy7jCz9GHZCvMEwrEOviKoHD3fBinTG0hFeZAtYeZA5lRZCZAOZC7NKh8jnaj6w4Qa9Ecui3K6MoDdZCMeoIzHvYidz9gJuGYTgZCLOkQIoFfOADMf1WKoQI8ZB25eiKDjTNXYGc3AnrkowLuAIKZBogZDZD') {
-		res.send(req.query['hub.challenge'])
+		res.status(200).send('EVENT_RECEIVED');
+	}else {
+		res.sendStatus(404);
 	}
-	res.send('Error, wrong token')
-})
+});
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+app.get('/webhook', (req, res) => {
+	
+	//agregamos token para verificar
+	let VERIFY_TOKEN = "<EAAEZBSgLMwiUBAKOCDL4o3HkDJy7jCz9GHZCvMEwrEOviKoHD3fBinTG0hFeZAtYeZA5lRZCZAOZC7NKh8jnaj6w4Qa9Ecui3K6MoDdZCMeoIzHvYidz9gJuGYTgZCLOkQIoFfOADMf1WKoQI8ZB25eiKDjTNXYGc3AnrkowLuAIKZBogZDZD>"
+
+	let mode = req.query['hub.mode'];
+	let token = req.query['hub.verify_token'];
+	let challenge = req.query['hub.challenge'];
+
+
+	if (mode && token){
+		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+			console.log('WEBHOOK_VERIFIED');
+			res.status(200).send(challenge);
+		} else {
+			res.sendStatus(403);
+		}
+	}
+});

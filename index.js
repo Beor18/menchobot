37a6615c822397cb4acb09ee60dc93e876d5b1c3
Fernando Lopez <fer.ragnar19@gmail.com>
2018-02-19 12:02:11 -0300

    agregamos el extremo del webhook

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ const
 	bodyParser = require('body-parser'),
 	app = express().use(bodyParser.json()); // creamos express server
 
-app.listen(process.env.PORT || 1337, () => console.log('webhook funciona!'))
+app.listen(process.env.PORT || 1337, () => console.log('webhook funciona!'));
+
+app.post('/webhook', (req, res) => {
+	let body = req.body;
+
+	if (body.object === 'page') {
+		body.entry.forEach(function(entry){
+			let webhook_event = entry.messaging[0];
+			console.log(webhook_event);
+		});
+
+		res.status(200).send('EVENT_RECEIVED');
+	}else {
+		res.sendStatus(404);
+	}
+});

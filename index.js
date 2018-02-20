98642ba45a6ebd1afe3ecc99fd907c62fededbd0
Fernando Lopez <fer.ragnar19@gmail.com>
2018-02-20 08:10:05 -0300

    cambios

--- index.js
+++ index.js
@@ -1,244 +1,92 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
+// Importar las dependencias para configurar el servidor
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
 
+var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 8080)); // Indispensable colocar el puerto 8080 para correr la aplicación en heroku
+// configurar el puerto y el mensaje en caso de exito
+app.listen((process.env.PORT || 5000), () => console.log('El servidor webhook esta escchando!'));
 
-// Imprimo un texto en el home de mi servidor Heroku para comprobar si mi deployment se hizo correctamente
-app.get('/', function (req, res) {
-    res.send('Hola soy un Bot de Facebook Messenger :)');
+// Ruta de la pagina index
+app.get("/", function (req, res) {
+    res.send("Se ha desplegado de manera exitosa el CMaquera ChatBot :D!!!");
 });
 
-// Conecto a mi Facebook Webhook
-app.get('/webhook', function (req, res) {
+// Facebook Webhook
 
-	const VERIFY_TOKEN = "<EAAEZBSgLMwiUBAKOCDL4o3HkDJy7jCz9GHZCvMEwrEOviKoHD3fBinTG0hFeZAtYeZA5lRZCZAOZC7NKh8jnaj6w4Qa9Ecui3K6MoDdZCMeoIzHvYidz9gJuGYTgZCLOkQIoFfOADMf1WKoQI8ZB25eiKDjTNXYGc3AnrkowLuAIKZBogZDZD>";
-
-    if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
-        res.send(req.query['hub.challenge']);
+// Usados para la verificacion
+app.get("/webhook", function (req, res) {
+    // Verificar la coincidendia del token
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+        // Mensaje de exito y envio del token requerido
+        console.log("webhook verificado!");
+        res.status(200).send(req.query["hub.challenge"]);
     } else {
-        res.send('Token de seguridad inválido');
+        // Mensaje de fallo
+        console.error("La verificacion ha fallado, porque los tokens no coinciden");
+        res.sendStatus(403);
     }
 });
 
-// Le muestro un mensaje de ayuda para usar el bot
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            if(introResponse(event.sender.id, event.message.text)) {
-                res.sendStatus(200);
-            }
-            else if(newResponselnk(event.sender.id, event.message.text)) {
-                res.sendStatus(200);
-            }
-            else if(newResponse(event.sender.id, event.message.text)) {
-                res.sendStatus(200);
-            }
-            else {
-              //Si el usuario escribe un mensaje le muestro una guia de uso del bot
-                sendMessage(event.sender.id, {text: "Por favor escribe el comando 'ayuda' (sin las comillas), para Ver los Comandos disponibles."});
-            }
-        }
+// Todos eventos de mesenger sera apturados por esta ruta
+app.post("/webhook", function (req, res) {
+    // Verificar si el vento proviene del pagina asociada
+    if (req.body.object == "page") {
+        // Si existe multiples entradas entraas
+        req.body.entry.forEach(function(entry) {
+            // Iterara todos lo eventos capturados
+            entry.messaging.forEach(function(event) {
+                if (event.message) {
+                    process_event(event);
+                }
+            });
+        });
+        res.sendStatus(200);
     }
-    res.sendStatus(200);
 });
 
-// Defino las palabras que puede usar el usuario dentro del Messenger para el comando 'busca blog' , ejemplo: 'buscar blog javascript'
-function newResponselnk(recipientId, text) {
-    text = text || "";
-    var buscar = text.match(/buscar/gi);
-    var ux = text.match(/ux/gi);
-    var blog = text.match(/blog/gi);
-    var php = text.match(/php/gi);
-    var android = text.match(/android/gi);
-    var javascript = text.match(/javascript/gi);
-    var python = text.match(/python/gi);
-    var ruby = text.match(/ruby/gi);
-    if(buscar != null && blog != null) {
-        var query = "";
 
-        //sendMessage(recipientId, message);
-        if(android != null) {
-            query = "Android";
-        } else if (javascript != null) {
-            query = "Javascript";
-        } else if (php != null) {
-            query = "PHP";
-        } else if (ux != null) {
-            query = "UX";
-        } else if (python != null) {
-            query = "Python";
-        } else if (ruby != null) {
-            query = "Ruby";
+// Funcion donde se procesara el evento
+function process_event(event){
+    // Capturamos los datos del que genera el evento y el mensaje 
+    var senderID = event.sender.id;
+    var message = event.message;
+    
+    // Si en el evento existe un mensaje de tipo texto
+    if(message.text){
+        // Crear un payload para un simple mensaje de texto
+        var response = {
+            "text": 'Enviaste este mensaje: ' + message.text
         }
-        sendButtonMessagelnk(recipientId, query);
-        return true
-    }
-    return false;
-};
-
-// Muestro un Boton el cual es un link al Blog de Platzi con los resultados obtenidos
-function sendButtonMessagelnk(recipientId, query) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "button",
-          text: "Resultados de "+query+":",
-          buttons:[{
-            type: "web_url",
-            url: "https://platzi.com/blog/?s="+query,
-            title: "Platzi: " + query
-          }]
-        }
-      }
-    }
-  };  
-
-  callSendAPI(messageData);
-}
-
-// Valido la conexion a la Graph API de Facebook
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Se ha enviado el mensaje generado con id %s a %s", 
-        messageId, recipientId);
-    } else {
-      console.error("No se puede enviar mensajes.");
-      console.error(response);
-      console.error(error);
-    }
-  });  
-}
-
-// Defino las respuestas para los comandos 'sobre platzi', 'ayuda'
-function introResponse(recipientId, text) {
-    text = text || "";
-
-    var sobre = text.match(/sobre/gi);
-    var platzi = text.match(/platzi/gi);
-    var ayuda = text.match(/ayuda/gi);
-    var testimonios = text.match(/testimonios/gi);
-
-    if(sobre != null && platzi != null) {
-        message = {
-            text: "Platzi es la mejor plataforma de habla hispana con Cursos profesionales de desarrollo web y de apps, marketing online, diseño de interfaces, servidores. Con clases en vivo y profesores sabios de la industria. Bienvenido :)."
-        }
-        sendMessage(recipientId, message);
-        return true;
-    }
-    if(ayuda != null) {
-        message = {
-            text: "Platzi Bot Ver. 1.0 "+ "\n" +" Comandos disponibles: "+ "\n" +" 1)Buscar blog Javascript, etc.(Puedes buscar Android, Javascript, PHP, UX, Python, Ruby) "+ "\n" +" 2)Sobre Platzi. "+ "\n" +" 3)Testimonios. "+ "\n" +" 4)Ayuda."
-        }
-        sendMessage(recipientId, message);
-        return true;
     }
     
-    return false;
-};
-
-
-// Defino el comando 'testimonios'
-function newResponse(recipientId, text) {
-    text = text || "";
-    var testimonios = text.match(/testimonios/gi);
-
-    if(testimonios != null) {
-        var query = "";
-
-        //sendMessage(recipientId, message);
-        if(testimonios != null) {
-            query = "Historias";
-        } 
-        sendButtonMessage(recipientId, query);
-        return true
-    }
-    return false;
-};
-
-// Muestro los los wildcards con los 'testimonios'
-function sendButtonMessage(recipientId, query) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "La Receta para mantenerse actualizado.",
-                    "subtitle": "Es increíble cómo la formación profesional técnica puede motivarte a participar y aportar en el proceso de...",
-                    "image_url": "https://static.platzi.com/static/stories/images/gollum-card.3cbc54914d6f.png",
-                    
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://platzi.com/historias/la-receta-para-mantenerse-actualizado/",
-                        "title": "Leer más (+)"
-                    }, {
-                        "type": "web_url",
-                        "url": "platzi.com/historias",
-                        "title": "Ver más Historias..."
-                    }],
-                }, {
-                    "title": "Platzi me ayudó a crecer y a aprender más de lo que ya sabía.",
-                    "subtitle": "Soy Vicky O’Shee de Argentina, trabajo como emprendedora digital ahora radicada en Medellín, Colombia...",
-                    "image_url": "https://static.platzi.com/static/stories/images/victoria-card.98c35c8ac6af.jpg",
-                    
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://platzi.com/historias/me-ayudo-a-crecer-y-a-aprender-mas-de-lo-que-ya-sabia/",
-                        "title": "Leer más (+)"
-                    }, {
-                        "type": "web_url",
-                        "url": "platzi.com/historias",
-                        "title": "Ver más Historias..."
-                    }],
-                }]
-            }
-      }
-    }
-  };  
-
-  callSendAPI(messageData);
+    // Enviamos el mensaje mediante SendAPI
+    enviar_texto(senderID, response);
 }
 
-// Envio mensajes en el Messenger
-function sendMessage(recipientId, message) {
+// Funcion donde el chat respondera usando SendAPI
+function enviar_texto(senderID, response){
+    // Construcicon del cuerpo del mensaje
+    let request_body = {
+        "recipient": {
+          "id": senderID
+        },
+        "message": response
+    }
+    
+    // Enviar el requisito HTTP a la plataforma de messenger
     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+          console.log('Mensaje enviado!')
+        } else {
+          console.error("No se puedo enviar el mensaje:" + err);
         }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error enviando el Mensaje: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
+    }); 
+}
